e94d9d8aff8b2f55a51a6f4a0512aa43ee8eca38
Clementine <wtan387@aucklanduni.ac.nz>
2025-01-22 18:31:00 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
 let emojis = [];
-let e1, e2, e3, e4;
+    let e1, e2, e3, e4;
+    let slider;
 
-function preload() {
-  e1 = loadImage('gif/m1.GIF');
-  e2 = loadImage('gif/m2.gif');
-  e3 = loadImage('gif/m3.gif');
-  e4 = loadImage('gif/m4.gif');
-}
+    function preload() {
+      e1 = loadImage('gif/m1.GIF');
+      e2 = loadImage('gif/m2.gif');
+      e3 = loadImage('gif/m3.gif');
+      e4 = loadImage('gif/m4.gif');
+    }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(225);
-  emojis.push(e1);
-  emojis.push(e2);
-  emojis.push(e3);
-  emojis.push(e4);
-  let button = createButton('click here');
-  button.position(10, 10);
-  button.mousePressed(() => {
-    let r = random(emojis);
-    image(r, random(width), random(height));
-  });
-}
+    function setup() {
+      createCanvas(windowWidth, windowHeight);
+      background(225);
+      emojis.push(e1);
+      emojis.push(e2);
+      emojis.push(e3);
+      emojis.push(e4);
+      let button = createButton('click here');
+      button.position(10, 10);
+      button.mousePressed(() => {
+        let r = random(emojis);
+        r.x = random(width);
+        r.y = random(height);
+        image(r, r.x, r.y);
+      });
+      slider = createSlider(0, 255, 255);
+      slider.position(100, 350);
+      slider.size(200);
+    }
 
-function draw() {
-}
+    function draw() {
+      background(225);
+      let alphaValue = slider.value();
+      for (let i = 0; i < emojis.length; i++) {
+        if (emojis[i].x!== undefined && emojis[i].y!== undefined) {
+          tint(255, alphaValue);
+          image(emojis[i], emojis[i].x, emojis[i].y);
+        }
+      }
+    }
